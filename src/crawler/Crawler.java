6bd476ac79880333df8a95758416b6922b80801f
CoffeeShelter <ij238950@gmail.com>
2021-11-14 21:32:54 +0900

    First Upload

---
+++ src/crawler/Crawler.java
@@ -0,0 +1,7 @@
+package crawler;
+
+public interface Crawler {
+	
+	// 상품 주소 가져오기
+	public String getProductUrl();
+}
---
+++ src/crawler/gmarket/GmarketCrawler.java
@@ -0,0 +1,14 @@
+package crawler.gmarket;
+
+import crawler.Crawler;
+
+public class GmarketCrawler implements Crawler{
+	
+	private static String mainURL = "";
+	
+	public String getProductUrl() {
+		String url = "";
+		
+		return url;
+	}
+}
